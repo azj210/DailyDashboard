b853fba0df657b8ed12eb6658cfb20ad85e861a9
ldj20 <lawrencedjiang@gmail.com>
2020-06-17 22:06:25 -0400

    Setup Multi-Path

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      
+      <Link to="/" className="nav-link">Home</Link>
+
+      { authenticated ? <Route path="/" exact={true} component={AccountHome} /> : <Route path="/" exact={true} component={Home} />}
     </div>
   );
 }
