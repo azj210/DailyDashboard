01cc5af40ba1cf5b627af731e85b6b76827c41bd
azj210 <ajiang1098@gmail.com>
2020-06-18 22:17:40 -0400

    changed app directory to backend in server

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get("/", (req, res) => {
     res.json({ message: "Welcome to Casual Decision Maker" });
 });
 
-require("./app/routes/user.routes.js")(app);
+require("./backend/routes/user.routes.js")(app);
 
 //set port, listen for requests
 const PORT = process.env.PORT || 3000;
