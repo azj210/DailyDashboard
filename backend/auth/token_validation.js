40926667947543a8c28d55829c88fa91a9a122a6
azj210 <ajiang1098@gmail.com>
2020-06-23 01:23:48 -0400

    code to check for bearer token

---
+++ backend/auth/token_validation.js
@@ -0,0 +1,28 @@
+const { verify } = require("jsonwebtoken");
+
+//need to use this token to validate certain services
+//apply this middleware in the apis that you need
+module.exports = {
+    checkToken: (req, res, next) => {
+        let token = req.get("authorization");
+        if(token) {
+            //remove the bearer from the token. The token starts from the 7 index
+            token = token.slice(7);
+            verify(token, "qwe1234", (err, decoded) => {
+                if (err){
+                    res.json({
+                        success: 0,
+                        message: "Invalid Token"
+                    });
+                } else {
+                    next();
+                }
+            });
+        } else {
+            res.json({
+                success: 0,
+                message: "Access Denied. Unauthorized User."
+            });
+        }
+    }
+}
