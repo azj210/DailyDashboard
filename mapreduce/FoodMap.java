8062ce9bd3f3c6120fd12b8ab0440afcd3df7597
azj210 <ajiang1098@gmail.com>
2020-07-04 01:06:21 -0400

    update food mapreduce

--- mapreduce/FoodMap.java
+++ mapreduce/FoodMap.java
@@ -3,26 +3,41 @@ import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
+
 import java.util.*;
 
 public class FoodMap
     extends Mapper<LongWritable, Text, Text, Text> {
 
-      @Override
-      public void map(LongWritable key, Text value, Context context)
+	@Override
+	public void map(LongWritable key, Text value, Context context)
       throws IOException, InterruptedException {
+    
+	    String[] line = value.toString().lower().split(";");
 
-          String[] line = value.toString().split(";");
+	    if (!line[0].equals("index")) {
 
-          //save macronutrients for each food item
-          if (!line[0].equals("index")){
-              String cName = line[11];
+	    	String food = line[-1].split(" ")[0];
 
-              String res = line[2] + ';' + line[3] + ';' + line[4] + ';' +
-                     line[5] + ';' + line[6] + ';' + line[7];
+	    	//eliminate the brand labled foods
+	    	if (!food.contains("'") && !food.matches("\\A\\p{ASCII}*\\z")) {
+	    		//handle the key
+	    		String f = line[-4];
+	    		//first possibility is if the subcategory is located in in the index after food name
+	    		if (!line[-3].isEmpty()) {
+	    			f += " " + line[-3];
+	    		}
+	    		//second possibility is if the subcategory is if first index after food name is null but then the second index after contains the subcategory
+	    		//but then the subcategory doesn't contains the word raw or uncooked 
+	    		else if (!line[-2].isEmpty() && (!line[-2].contains("raw") && !line[-2].contains("uncooked"))) {
+	    			f += " " + line[-2];
+	    		}
 
-              context.write(new Text(cName), new Text(res));
+	    		//handle the value
+	    		String val = line[2] + ";" + line[3] + ";" + line[4] + ";" + line[5] + ";" + line[6] + ";" line[7];
 
-          }
-      }
-    }
+	    		context.write(new Text(f), new Text(val));
+	    	}
+	    }	
+	}
+}
--- mapreduce/FoodReducer.java
+++ mapreduce/FoodReducer.java
@@ -1,54 +1,43 @@
 import java.io.IOException;
 import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
+
+import java.util.*;
 import java.lang.Math;
 
 public class FoodReducer
     extends Reducer<Text, Text, Text, Text> {
-
+  
   @Override
   public void reduce(Text key, Iterable<Text> values, Context context)
       throws IOException, InterruptedException {
+    
+    //array holding the value contents. The respective indicies are:
+    //serving size, calorie, fat, protein, carbohydrate, sugar
+    double vals[] = new double[6];
+    Arrays.fill(vals, 0);
+    int n = 0;
 
-          double serve_size_avg = 0;
-          double calorie_avg = 0;
-          double fat_avg = 0;
-          double protein_avg = 0;
-          double carb_avg = 0;
-          double sugar_avg = 0;
-          double num_items = 0;
+    for (Text value : values) {
 
-          for (Text value : values) {
-              String[] line = value.toString().split(";");
+        String[] calc = value.toString().split(";");
+        //add the totals for each field to the respective index in vals
+        for (int i = 0; i < vals.length; i ++) {
+            vals[i] += Double.parseDouble(calc[i]);
+        }
+        n += 1;
+    }
 
-              // add value to total
-              serve_size_avg += Double.parseDouble(line[0]);
-              calorie_avg += Double.parseDouble(line[1]);
-              fat_avg += Double.parseDouble(line[2]);
-              protein_avg += Double.parseDouble(line[3]);
-              carb_avg += Double.parseDouble(line[4]);
-              sugar_avg += Double.parseDouble(line[5]);
+    String res = ""
+    //calculate the average of each field and round to 2 decimal places and add it to the output string
+    for (int i = 0; i < vals.length; i++) {
+        res += Double.toString(Math.round((vals[i] / n) * 100 / 100)) + ";";
+    }
 
-              // count number of food items to get average
-              num_items += 1;
-          }
-
-          // calculate average and round to 2 decimal points
-          serve_size_avg = Math.round(serve_size_avg/num_items*100)/100;
-          calorie_avg = Math.round(calorie_avg/num_items*100)/100;
-          fat_avg = Math.round(fat_avg/num_items*100)/100;
-          protein_avg = Math.round(protein_avg/num_items*100)/100;
-          carb_avg = Math.round(carb_avg/num_items*100)/100;
-          sugar_avg = Math.round(sugar_avg/num_items*100)/100;
-
-          String avgs = Double.toString(serve_size_avg) + ";" + Double.toString(calorie_avg) + ";" +
-        		  Double.toString(fat_avg) + ";" + Double.toString(protein_avg) + ";" +
-        		  Double.toString(carb_avg) + ";" + Double.toString(sugar_avg);
-
-          // key is food_key and value is all the averages
-          context.write(key, new Text(avgs));
-
-  		}
+    //make sure to remove the extra ; at the end
+    context.write(key, new Text(res.substring(0, res.length() - 1)));
   }
+}
+
+
